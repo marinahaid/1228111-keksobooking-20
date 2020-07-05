b1d2f61c3a0c59e58a937a79836515669a02c828
marinahaid <60192957+marinahaid@users.noreply.github.com>
2020-07-05 22:10:20 +0300

    m3-t3 m7-t1

---
+++ js/data.js
@@ -0,0 +1,60 @@
+'use strict';
+
+(function () {
+var TITLE_NAMES = ['Большая уютная квартира', 'Маленькая неутная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгалао по колено в воде']
+var OBJECT_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var OBJECT_PRICE = ['any', 'middle', 'low', 'high'];
+var HOUSING_ROOMS = ['any', '2', '2', '3'];
+var HOUSING_GUESTS = ['any', '2', '1', '0'];
+
+var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES_SERVICES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+
+var filters = [];
+var updateFilters = function () {
+  var sameTypeFilters = filters.filter(function (it) {
+    return it
+  })
+}
+
+var getRandomElement = function(array) {
+  var randomElementIndex = Math.floor(Math.random() * array.length);
+  return array[randomElementIndex];
+};
+
+var filterElement = document.querySelector('.map__filters');
+
+var filterTypeElement = filterElement.querySelector('#housing-type');
+filterTypeElement.addEventListener.addEventListener('click', function() {
+  var newType = getRandomElement(OBJECT_TYPE);
+  this.
+})
+
+var filterPriceElement = filterElement.querySelector('#housing-price');
+filterPriceElement.addEventListener.addEventListener('click', function() {
+  var newPrice = getRandomElement(OBJECT_PRICE);
+  this.
+})
+
+var filterRoomElement = filterElement.querySelector('#housing-rooms');
+filterRoomElement.addEventListener.addEventListener('click', function() {
+  var newRoom = getRandomElement(HOUSING_ROOMS);
+  this.
+})
+
+
+var filterGuestElement = filterElement.querySelector('#housing-guests');
+filterGuestElement.addEventListener.addEventListener('click', function() {
+  var newGuest = getRandomElement(HOUSING_GUESTS);
+  this.
+})
+
+var filterFeatureElement = filterElement.querySelector('#housing-guests');
+filterFeatureElement.addEventListener.addEventListener('click', function() {
+  var newFeature = getRandomElement(FEATURES_SERVICES);
+  this.
+})
+
+})();
