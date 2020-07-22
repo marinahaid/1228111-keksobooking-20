faf720031eb6889eb0347db42cd056f2558992f2
marinahaid <60192957+marinahaid@users.noreply.github.com>
2020-07-22 21:46:45 +0300

    xhr.js

--- js/xhr.js
+++ js/xhr.js
@@ -1,14 +1,13 @@
 'use strict';
 
 (function () {
-  var URL = 'https://javascript.pages.academy/keksobooking/data';
-  window.load = function (data, onSuccess, onError) {
+  var URL__LOAD = 'https://javascript.pages.academy/keksobooking/data';
+  var URL__UPLOAD =  'https://javascript.pages.academy/keksobooking';
+
+  function load (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
 
-
-    var form = document.querySelector('.ad-form');
-
-    xhr.responseType = 'json';
+ xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
@@ -28,17 +27,38 @@
 
     xhr.timeout = 10000;
 
-    xhr.open('GET', URL);
+    xhr.open('GET', URL__LOAD);
     xhr.send();
+  };
 
+function upload (data, onSuccess, onError) {
+  var xhr = new XMLHttpRequest ();
 
-    xhr.open('POST', URL);
+  xhr.responseType = 'json';
+
+  xhr.addEventListener('load', function () {
+    if (xhr.status === 200) {
+      onSuccess(xhr.response);
+    } else {
+      onError('Статус ответа:' + xhr.status + '' + xhr.statusText);
+    }
+  });
+
+  xhr.addEventListener('error', function () {
+onError('Произошла ошибка соединения');
+  });
+  xhr.addEventListener('timeout', function () {
+onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+});
+
+xhr.timeout = 10000;
+
+ xhr.open('POST', URL__UPLOAD);
     xhr.send(data);
-
+};
 window.xhr = {
   load: load,
   upload: upload
 }
-};
 })();
 
